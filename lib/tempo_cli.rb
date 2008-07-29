0a10a3a275a7e754072c79a715b750f140a40f56
James Golick <james@giraffesoft.ca>
2008-07-29 16:44:07 -0400

    add basic tempo CLI

--- lib/tempo_cli.rb
+++ lib/tempo_cli.rb
@@ -1,6 +1,24 @@
+$LOAD_PATH << File.dirname(__FILE__)
 require 'rubygems'
-require 'activeresource'
+require 'activesupport'
+require 'rest_client'
 
 module TempoCli
+  CONFIG_FILE = ENV['HOME']+'/.tempo'
   
+  class << self
+    attr_accessor :username, :password
+    
+    def create_entry(command)
+      get_credentials!
+      resource = RestClient::Resource.new 'https://app.keeptempo.com/entries', username, password
+      resource.post({:command => command}.to_xml(:root => 'entry'), :content_type => 'application/xml', :accept => 'application/xml')
+    end
+    
+    def get_credentials!
+      abort "You must create a #{CONFIG_FILE} file to use this CLI." unless File.exist?(CONFIG_FILE)
+      config = YAML.load(File.read(CONFIG_FILE)).symbolize_keys
+      @username, @password = config[:username], config[:password]
+    end
+  end
 end
